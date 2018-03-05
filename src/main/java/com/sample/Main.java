0cecece47caa113ff48886e1041e456a1943f26f
alexandrestrevenski <alexandre.strevenski@gmail.com>
2018-03-04 21:06:57 -0300

    Sample with JDBI

---
+++ src/main/java/com/sample/Main.java
@@ -0,0 +1,44 @@
+package com.sample;
+
+import org.jdbi.v3.core.Jdbi;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Jdbi jdbi = Jdbi.create("jdbc:h2:mem:test"); // (H2 in-memory database)
+
+        List<User> users = jdbi.withHandle(handle -> {
+            handle.execute("CREATE TABLE user (id INTEGER PRIMARY KEY, name VARCHAR)");
+
+            // Inline positional parameters
+            handle.execute("INSERT INTO user(id, name) VALUES (?, ?)", 0, "Alice");
+
+            // Positional parameters
+            handle.createUpdate("INSERT INTO user(id, name) VALUES (?, ?)")
+                    .bind(0, 1) // 0-based parameter indexes
+                    .bind(1, "Bob")
+                    .execute();
+
+            // Named parameters
+            handle.createUpdate("INSERT INTO user(id, name) VALUES (:id, :name)")
+                    .bind("id", 2)
+                    .bind("name", "Clarice")
+                    .execute();
+
+            // Named parameters from bean properties
+            handle.createUpdate("INSERT INTO user(id, name) VALUES (:id, :name)")
+                    .bindBean(new User(3, "David"))
+                    .execute();
+
+            // Easy mapping to any type
+            return handle.createQuery("SELECT * FROM user ORDER BY name")
+                    .map((rs, ctx) -> new User(rs.getInt("id"), rs.getString("name")))
+                    .list();
+
+        });
+
+        users.forEach(user -> System.out.println(user.getName()));
+    }
+}
